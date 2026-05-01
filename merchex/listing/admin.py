9854d8c39f5da4f085a3621bd4d7aa48d79282fd
Maxime <jlv.cordier75@gmail.com>
2026-05-01 11:12:15 +0200

    Implement models and admin configurations for Band and Listing, including migrations for new fields

--- merchex/listing/admin.py
+++ merchex/listing/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from listing.models import Band, Listing
 
-# Register your models here.
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre') 
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'band', 'year', 'sold')
+
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
--- merchex/listing/models.py
+++ merchex/listing/models.py
@@ -1,9 +1,42 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        ROCK = 'Rock'
+        POP = 'Pop'
+        JAZZ = 'Jazz'
+        HIP_HOP = 'HP'
+        ELECTRONIC = 'Elec'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=5, choices=Genre.choices)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2023)])
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Listing(models.Model):
+
+    class Type(models.TextChoices):
+        RECORD = 'Record',
+        CLOTHING = 'Clothing',
+        POSTER = 'Poster',
+        MISCELLANEOUS = 'Miscellaneous'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(null=True, blank=True)
+    type = models.fields.CharField(choices=Type.choices, max_length=20)
+
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.title}'
 
     
